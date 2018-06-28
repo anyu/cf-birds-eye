500c3be076c32dedf06791501ae54b9088960f5c
An Yu <anyu@pivotal.io>
2018-06-27 17:07:01 -0700

    Retrieve and display list of orgs; get raw spaces dump for one org
    - no handling of spaceless orgs
    - delete outdated binary

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"code.cloudfoundry.org/cli/plugin"
+	"code.cloudfoundry.org/cli/plugin/models"
 )
 
 const pluginName = "birds-eye"
@@ -12,7 +14,58 @@ type BirdsEye struct{}
 
 func (c *BirdsEye) Run(cliConnection plugin.CliConnection, args []string) {
 	if args[0] == pluginName {
-		fmt.Println("Running the command")
+
+		var (
+			err error
+
+			isLoggedIn bool
+			orgs       []plugin_models.GetOrgs_Model
+			spaces     []plugin_models.GetSpaces_Model
+			apps       []plugin_models.GetAppsModel
+
+			orgNames []string
+			// spaceNames []string
+			appNames []string
+
+			orgGUIDs []string
+		)
+
+		if _, err = cliConnection.HasAPIEndpoint(); err != nil {
+			fmt.Println("No API endpoint set")
+		}
+
+		if isLoggedIn, err = cliConnection.IsLoggedIn(); err != nil {
+			fmt.Printf("Logged in? %t", isLoggedIn)
+		}
+
+		if orgs, err = cliConnection.GetOrgs(); err != nil {
+			fmt.Printf("Error getting orgs: %v", orgs)
+		}
+
+		if spaces, err = cliConnection.GetSpaces(); err != nil {
+			fmt.Printf("Error getting spaces: %v", spaces)
+		}
+
+		if apps, err = cliConnection.GetApps(); err != nil {
+			fmt.Printf("Error getting apps: %v", apps)
+		}
+
+		for _, org := range orgs {
+			orgNames = append(orgNames, org.Name)
+			orgGUIDs = append(orgGUIDs, org.Guid)
+		}
+
+		fmt.Print("All orgs:\n\n", strings.Join(orgNames, "\n"))
+
+		url := fmt.Sprintf("/v2/organizations/%s/spaces", orgGUIDs[0])
+
+		orgSpaces, err := cliConnection.CliCommandWithoutTerminalOutput("curl", url)
+		if err != nil {
+			fmt.Printf("Error getting spaces from org: %s", orgNames[0])
+		}
+
+		fmt.Print("All spaces:\n\n", strings.Join(orgSpaces, "\n"))
+		fmt.Print("All apps:\n\n", strings.Join(appNames, "\n"))
 	}
 }
 
