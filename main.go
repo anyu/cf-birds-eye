1004de358e79c9be6723e41f952205c162400097
An Yu <anyu@pivotal.io>
2018-09-18 18:49:59 -0400

    Display apps in each space; correct order but messy

--- main.go
+++ main.go
@@ -18,13 +18,30 @@ type Org struct {
 }
 
 type OrgResources struct {
-	Entity OrgEntity `json:"entity"`
+	Entity   OrgEntity   `json:"entity"`
+	Metadata OrgMetadata `json:"metadata"`
 }
 
 type OrgEntity struct {
 	Name             string `json:"name"`
 	OrganizationGUID int    `json:"organization_guid"`
 	AppsURL          string `json:"apps_url"`
+}
+
+type OrgMetadata struct {
+	GUID string `json:"guid"`
+}
+
+type Space struct {
+	Resources []SpaceResources `json:"resources"`
+}
+
+type SpaceResources struct {
+	Entity SpaceEntity `json:"entity"`
+}
+
+type SpaceEntity struct {
+	Name string `json:"name"`
 }
 
 func (c *BirdsEye) Run(cliConnection plugin.CliConnection, args []string) {
@@ -36,11 +53,8 @@ func (c *BirdsEye) Run(cliConnection plugin.CliConnection, args []string) {
 			isLoggedIn bool
 			orgs       []plugin_models.GetOrgs_Model
 			spaces     []plugin_models.GetSpaces_Model
-			apps       []plugin_models.GetAppsModel
 
 			orgNames []string
-			// spaceNames []string
-			appNames []string
 		)
 
 		if _, err = cliConnection.HasAPIEndpoint(); err != nil {
@@ -59,35 +73,36 @@ func (c *BirdsEye) Run(cliConnection plugin.CliConnection, args []string) {
 			fmt.Printf("Error getting spaces: %v", spaces)
 		}
 
-		if apps, err = cliConnection.GetApps(); err != nil {
-			fmt.Printf("Error getting apps: %v", apps)
-		}
-
 		for _, org := range orgs {
 			orgNames = append(orgNames, org.Name)
 		}
-		fmt.Print("All orgs:\n\n*s", strings.Join(orgNames, "\n"))
+		fmt.Print("All orgs:\n\n", strings.Join(orgNames, "\n"))
 
 		for _, org := range orgs {
 			var orgResult Org
 			url := fmt.Sprintf("/v2/organizations/%s/spaces", org.Guid)
 			orgResult = c.UnmarshalOrg(url, cliConnection)
 
-			var orgSpaces, appsURLsInSpace []string
+			var orgSpaces []string
 			for _, space := range orgResult.Resources {
 				orgSpaces = append(orgSpaces, space.Entity.Name)
-				appsURLsInSpace = append(appsURLsInSpace, space.Entity.AppsURL)
 			}
-
 			fmt.Print("\n\n")
-			fmt.Printf("All spaces in %s:\n\n* %s", org.Name, strings.Join(orgSpaces, "\n"))
+			fmt.Printf("All spaces in %s:\n\n%s", org.Name, strings.Join(orgSpaces, "\n"))
 
-			for _, app := range apps {
-				appNames = append(appNames, app.Name)
+			for _, space := range orgResult.Resources {
+				var spaceResult Space
+
+				getSpaceAppsRequest := fmt.Sprintf("/v2/spaces/%s/apps", space.Metadata.GUID)
+				spaceResult = c.UnmarshalSpace(getSpaceAppsRequest, cliConnection)
+
+				var appsInSpace []string
+				for _, app := range spaceResult.Resources {
+					appsInSpace = append(appsInSpace, app.Entity.Name)
+				}
+				fmt.Print("\n\n")
+				fmt.Printf("All apps in %s:\n\n%s", space.Entity.Name, strings.Join(appsInSpace, "\n"))
 			}
-
-			// fmt.Print("\n\n")
-			// fmt.Print("All apps:\n\n", strings.Join(appNames, "\n"))
 		}
 	}
 }
@@ -120,6 +135,16 @@ func (c BirdsEye) UnmarshalOrg(url string, cliConnection plugin.CliConnection) O
 	json.Unmarshal([]byte(strings.Join(result, "")), &orgResult)
 
 	return orgResult
+}
+
+func (c BirdsEye) UnmarshalSpace(url string, cliConnection plugin.CliConnection) Space {
+	var spaceResult Space
+	cmd := []string{"curl", url}
+
+	result, _ := cliConnection.CliCommandWithoutTerminalOutput(cmd...)
+	json.Unmarshal([]byte(strings.Join(result, "")), &spaceResult)
+
+	return spaceResult
 }
 
 func main() {
